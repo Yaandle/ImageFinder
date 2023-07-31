04e76f22b0ac161f216797221f47d49e278553c9
Yaandle <131413645+Yaandle@users.noreply.github.com>
2023-08-01 09:46:06 +1000

    App code intergration
    Intergrated the old code that pulls the Roboflow dataset from the site with Ultralytics model.

--- Test.py
+++ Test.py
@@ -3,23 +3,22 @@ import shutil
 from ultralytics import YOLO
 import streamlit as st
 import zipfile
+import base64
 
-#Streamlit
-st.title('Image Object Detection')
-st.header('Enter a rider number to return images within a folder')
-class_name = st.text_input("0-999")
-
-model = YOLO("yolov8m.pt")
-folder_path = 'E:/Machine Learning/Object Detection/App 2.0/valid/images'
-destination_folder = 'filtered_images'
+model = YOLO("")
+folder_path = ''
+destination_folder = ''
 filtered_images = []
+
+class_name = st.text_input("Enter the desired class name")
+
 for image_name in os.listdir(folder_path):
     image_path = os.path.join(folder_path, image_name)
 
-    results = model(image_path)
+    results = model(image_path, conf=0.1)
     boxes = results[0].boxes
     for box in boxes:
-        if box.cls == 23:
+        if box.cls == class_name:
             filtered_images.append(image_path)
             source_file = os.path.join(folder_path, image_name)
             dest_file = os.path.join(destination_folder, image_name)
@@ -27,30 +26,15 @@ for image_name in os.listdir(folder_path):
             break
 
 
+for image_path in filtered_images:
+    shutil.copy2(image_path, destination_folder)
 
-def create_zip_file(source_folder, destination_zip_path):
-    with zipfile.ZipFile(destination_zip_path, 'w') as zipf:
-        for root, _, files in os.walk(source_folder):
-            for file in files:
-                zipf.write(os.path.join(root, file), file)
+zip_filename = 'filtered_images.zip'
+with zipfile.ZipFile(zip_filename, 'w') as zipf:
+    for root, dirs, files in os.walk(destination_folder):
+        for file in files:
+            zipf.write(os.path.join(root, file), file)
 
-def main():
-    st.title("Image Filter and Zip Downloader")
-
-    destination_folder = "filtered_images"
-    
-    if not os.path.exists(destination_folder):
-        st.warning("The 'filtered_images' folder does not exist. Please add the filtered images to the folder and re-run the app.")
-        return
-
-    create_zip_file(destination_folder, "filtered_images.zip")
-
-    st.success("Filtered images zip folder created!")
-    st.download_button(
-        label="Download Zip",
-        data=open("filtered_images.zip", "rb").read(),
-        file_name="filtered_images.zip",
-    )
-
-if __name__ == "__main__":
-    main()
+b64_zip_data = base64.b64encode(open(zip_filename, 'rb').read()).decode()
+download_link = f'<a href="data:application/zip;base64,{b64_zip_data}" download="{zip_filename}">Download Filtered Images</a>'
+st.markdown(download_link, unsafe_allow_html=True)
