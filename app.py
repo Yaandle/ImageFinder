22801f7976e4151e1eae8de878f235c83ce1bb01
Yaandle <131413645+Yaandle@users.noreply.github.com>
2023-08-20 18:59:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,61 @@
 import os
 import shutil
-from ultralytics import YOLO
-import streamlit as st
 import zipfile
 import base64
+from ultralytics import YOLO
+import streamlit as st
 
-model = YOLO("")
-folder_path = ''  #The folder the model will loop through
-destination_folder = 'filtered_images' #The folder images will be filtered too
-filtered_images = []
+# Load the YOLO model
+model = YOLO('')
 
-for image_name in os.listdir(folder_path):
-    image_path = os.path.join(folder_path, image_name)
+def main():
+    st.title  ("Image Filter, Copy, and Download")
+    rider_number = st.text_input  ("Enter a rider number, 0-999")
+    if rider_number:
+        try:
+            class_name = int(rider_number)
+        except: ValueError:st.write("Please enter number. ")
+        
+    folder_path = st.text_input  ("Enter the path to the source folder:")
+    destination_folder = 'filtered_images'
+    os.makedirs(destination_folder, exist_ok=True)
+    filtered_images = []
 
-    results = model(image_path, conf=0.1,)
-    boxes = results[0].boxes
-    for box in boxes:
-        if box.cls == #57:
-            filtered_images.append(image_path)
-            source_file = os.path.join(folder_path, image_name)
-            dest_file = os.path.join(destination_folder, image_name)
-            shutil.copy2(source_file, dest_file)
-            break
-    
+    if st.button  ("Filter and Copy Images"):
+        if os.path.exists(folder_path):
+            for image_name in os.listdir(folder_path):
+                image_path = os.path.join(folder_path, image_name)
 
-for image_path in filtered_images:
-    shutil.copy2(image_path, destination_folder)
+                results = model(image_path, conf=0.2)
+                boxes = results[0].boxes
 
-zip_filename = 'filtered_images.zip'
-with zipfile.ZipFile(zip_filename, 'w') as zipf:
-    for root, dirs, files in os.walk(destination_folder):
-        for file in files:
-            zipf.write(os.path.join(root, file), file)
+                for box in boxes:
+                    if box.cls == class_name:  
+                        filtered_images.append(image_path)
+                        source_file = os.path.join(folder_path, image_name)
+                        dest_file = os.path.join(destination_folder, image_name)
+                        shutil.copy2(source_file, dest_file)
+                        break
 
-b64_zip_data = base64.b64encode(open(zip_filename, 'rb').read()).decode()
-download_link = f'<a href="data:application/zip;base64,{b64_zip_data}" download="{zip_filename}">Download Filtered Images</a>'
-st.markdown(download_link, unsafe_allow_html=True)
+            st.write ("Filtered images copied to 'filtered_images' folder:")
+            for image in filtered_images:
+                st.image(image, caption=image, use_column_width=True)
+
+            for image_path in filtered_images:
+                shutil.copy2(image_path, destination_folder)
+
+            zip_filename = 'filtered_images.zip'
+            with zipfile.ZipFile(zip_filename, 'w') as zipf:
+                for root, dirs, files in os.walk(destination_folder):
+                    for file in files:
+                        zipf.write(os.path.join(root, file), file)
+
+            b64_zip_data = base64.b64encode(open(zip_filename, 'rb').read()).decode()
+            download_link = f'<a href="data:application/zip;base64,{b64_zip_data}" download="{zip_filename}">Download Filtered Images</a>'
+            st.markdown(download_link, unsafe_allow_html=True)
+
+        else:
+            st.write("Invalid source folder path. Please enter a valid path.")
+        
+if __name__ == "__main__":
+    main()
