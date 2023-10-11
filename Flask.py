de9527737e19a532a026bfbca5aa8c25113fabc9
Yaandle <131413645+Yaandle@users.noreply.github.com>
2023-10-11 22:07:40 +1100

    Update Flask.py (#59)

--- Flask.py
+++ Flask.py
@@ -6,15 +6,24 @@ from ultralytics import YOLO
 from google.cloud import storage
 import shutil
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
-model = YOLO('model1800.pt')
+
+CORS(app, resources={r"/webhook": {"origins": "http://localhost:3000"}})
+
+#Setup
+model = YOLO('MODEL3200.pt')
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "googlekey.json"
 source_bucket_name = 'odis-bucket'  
 destination_bucket_name = 'filtered-images-bucket'  
 storage_client = storage.Client()
 source_bucket = storage_client.bucket(source_bucket_name)
 destination_bucket = storage_client.bucket(destination_bucket_name)
+num_list = ['1', '10', '100', '103', '104', '105', '106', '11', '111', '112', '113', '114', '115', '117', '118', '12', '123', '128', '13', '132', '133', '137', '14', '145', '147', '15', '150', '151', '153', '159', '16', '160', '162', '164', '165', '166', '17', '171', '172', '174', '178', '18', '180', '182', '189', '19', '191', '199', '2', '20', '202', '204', '209', '21', '210', '211', '215', '217', '22', '222', '225', '226', '227', '23', '231', '235', '238', '24', '241', '243', '247', '25', '252', '257', '26', '266', '267', '27', '270', '272', '273', '275', '277', '28', '286', '29', '290', '291', '295', '298', '299', '3', '30', '309', '31', '310', '311', '313', '315', '317', '318', '32', '323', '325', '33', '338', '340', '346', '348', '35', '355', '36', '37', '376', '38', '39', '392', '394', '4', '40', '404', '41', '410', '411', '414', '42', '421', '427', '428', '43', '44', '46', '47', '474', '48', '480', '49', '5', '50', '504', '51', '514', '518', '521', '53', '532', '547', '549', '55', '555', '557', '56', '57', '58', '585', '59', '599', '6', '609', '61', '612', '613', '616', '62', '622', '65', '650', '66', '666', '673', '687', '690', '7', '71', '710', '711', '72', '724', '725', '74', '742', '77', '775', '782', '789', '79', '8', '80', '81', '818', '82', '823', '826', '83', '84', '85', '852', '86', '875', '876', '877', '88', '89', '893', '9', '914', '915', '916', '95', '952', '96', '97', '972', '99', '997', '999']
+
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     webhook_data = request.get_json()
@@ -28,8 +37,8 @@ def webhook():
                         print(f"Bike number: {bike_number}")
                         break 
     if bike_number:
-        data = {'Bike Number': bike_number}
-        gcp_url = 'https://odis-401208.ts.r.appspot.com'  
+        data = {'Bike Number': bike_number}  
+        gcp_url = 'https://odis-401208.ts.r.appspot.com'
         with requests.post(gcp_url + '/filter_images', json=data, timeout=5) as response:
             if response.status_code == 200:
                 print(f"Bike number '{bike_number}' sent to Flask backend successfully on GCP.")
@@ -40,7 +49,7 @@ def webhook():
     return jsonify({'message': 'Webhook received successfully'}), 200
 
 
-num_list = ['1', '10', '100', '104', '105', '106', '11', '112', '113', '115', '118', '12', '123', '128', '13', '132', '137', '14', '147', '15', '150', '151', '159', '16', '160', '162', '166', '17', '171', '172', '174', '178', '18', '180', '182', '189', '199', '2', '20', '202', '21', '210', '211', '215', '217', '22', '222', '226', '23', '235', '24', '25', '26', '266', '27', '273', '275', '277', '28', '286', '29', '291', '299', '3', '30', '31', '310', '313', '32', '323', '33', '340', '346', '348', '355', '36', '37', '376', '38', '39', '392', '4', '40', '404', '411', '414', '42', '421', '427', '428', '43', '44', '46', '47', '48', '5', '50', '51', '518', '521', '53', '549', '55', '57', '585', '59', '6', '609', '61', '616', '62', '65', '666', '7', '710', '72', '724', '74', '77', '79', '8', '80', '81', '818', '826', '85', '852', '88', '89', '893', '9', '915', '916', '952', '96', '97', '972', '99', '999']
+
 @app.route('/filter_images', methods=['POST'])
 def filter_images():
     if "Bike Number" not in request.json:
@@ -54,7 +63,6 @@ def filter_images():
             class_name = position
         except ValueError:
             return jsonify({"error": "Invalid rider number."}), 400
-
     if class_name is not None:
         filtered_images = []
         temp_dir = './temp_images'
@@ -76,7 +84,6 @@ def filter_images():
                         temp_image_path = os.path.join(temp_dir, 'temp_image.jpg')
                         source_blob.download_to_filename(temp_image_path)
                         destination_blob.upload_from_filename(temp_image_path)
-
                         processed_images_count += 1
                         if processed_images_count >= max_processed_images:
                             break
@@ -89,6 +96,7 @@ def filter_images():
         return jsonify(response_data)
     else:
         return "Rider number not found in the list."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
