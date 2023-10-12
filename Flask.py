738b64aba531ebfec3d2f2b769de94b8bebd336c
Yaandle <131413645+Yaandle@users.noreply.github.com>
2023-10-12 14:04:41 +1100

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -6,14 +6,16 @@ from ultralytics import YOLO
 from google.cloud import storage
 import shutil
 import requests
-from flask_cors import CORS
-
+from google.oauth2 import service_account
 app = Flask(__name__)
 
-CORS(app, resources={r"/webhook": {"origins": "http://localhost:3000"}})
 
 #Setup
-model = YOLO('MODEL3200.pt')
+model = YOLO('E:\Machine Learning\Object Detection\App IV\models\MODEL3200.pt')
+credentials = service_account.Credentials.from_service_account_file(
+    'E:/Machine Learning/Object Detection/App IV/backend/googlekey.json',
+    scopes=['https://www.googleapis.com/auth/cloud-platform']
+)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "googlekey.json"
 source_bucket_name = 'odis-bucket'  
 destination_bucket_name = 'filtered-images-bucket'  
@@ -23,31 +25,26 @@ destination_bucket = storage_client.bucket(destination_bucket_name)
 num_list = ['1', '10', '100', '103', '104', '105', '106', '11', '111', '112', '113', '114', '115', '117', '118', '12', '123', '128', '13', '132', '133', '137', '14', '145', '147', '15', '150', '151', '153', '159', '16', '160', '162', '164', '165', '166', '17', '171', '172', '174', '178', '18', '180', '182', '189', '19', '191', '199', '2', '20', '202', '204', '209', '21', '210', '211', '215', '217', '22', '222', '225', '226', '227', '23', '231', '235', '238', '24', '241', '243', '247', '25', '252', '257', '26', '266', '267', '27', '270', '272', '273', '275', '277', '28', '286', '29', '290', '291', '295', '298', '299', '3', '30', '309', '31', '310', '311', '313', '315', '317', '318', '32', '323', '325', '33', '338', '340', '346', '348', '35', '355', '36', '37', '376', '38', '39', '392', '394', '4', '40', '404', '41', '410', '411', '414', '42', '421', '427', '428', '43', '44', '46', '47', '474', '48', '480', '49', '5', '50', '504', '51', '514', '518', '521', '53', '532', '547', '549', '55', '555', '557', '56', '57', '58', '585', '59', '599', '6', '609', '61', '612', '613', '616', '62', '622', '65', '650', '66', '666', '673', '687', '690', '7', '71', '710', '711', '72', '724', '725', '74', '742', '77', '775', '782', '789', '79', '8', '80', '81', '818', '82', '823', '826', '83', '84', '85', '852', '86', '875', '876', '877', '88', '89', '893', '9', '914', '915', '916', '95', '952', '96', '97', '972', '99', '997', '999']
 
 
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    webhook_data = request.get_json()
-    bike_number = None
-    if 'line_items' in webhook_data:
-        for item in webhook_data['line_items']:
-            if 'properties' in item:
-                for prop in item['properties']:
-                    if 'name' in prop and prop['name'] == 'Bike Number' and 'value' in prop:
-                        bike_number = prop['value']
-                        print(f"Bike number: {bike_number}")
-                        break 
+    # Get the user input for bike number
+    bike_number = request.json.get('bike_number')
+
+
     if bike_number:
-        data = {'Bike Number': bike_number}  
-        gcp_url = 'https://odis-401208.ts.r.appspot.com'
-        with requests.post(gcp_url + '/filter_images', json=data, timeout=5) as response:
+        data = {'Bike Number': bike_number}
+        
+        # Update the URL to match your Streamlit URL
+        streamlit_url = 'http://127.0.0.1:5000'
+        with requests.post(f'{streamlit_url}/filter_images', json=data, timeout=5) as response:
             if response.status_code == 200:
                 print(f"Bike number '{bike_number}' sent to Flask backend successfully on GCP.")
             else:
                 print(f"Failed to send bike number '{bike_number}' to Flask backend on GCP.")
     else:
-        print("No 'Bike Number' properties found in the JSON data.")
+        print("No 'Bike Number' provided in the user input.")
+    
     return jsonify({'message': 'Webhook received successfully'}), 200
-
 
 
 @app.route('/filter_images', methods=['POST'])
