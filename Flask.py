76ff5ab08fdb9f1632ad52aee0b0260e41c08924
Yaandle <131413645+Yaandle@users.noreply.github.com>
2023-11-06 17:36:19 +1100

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, make_response, request, jsonify
-import os
+from flask import Flask, render_template, request, jsonify, make_response
 import time
 from google.cloud import storage
+import os
+import json
+from google.oauth2 import service_account
 
 app = Flask(__name__)
 
+def get_storage_client():
+    creds_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    if creds_json:
+        creds_info = json.loads(creds_json)
+        credentials = service_account.Credentials.from_service_account_info(creds_info)
+        return storage.Client(credentials=credentials)
+    else:
+        return storage.Client()
+
 def format_server_time():
-  server_time = time.localtime()
-  return time.strftime("%I:%M:%S %p", server_time)
+    server_time = time.localtime()
+    return time.strftime("%I:%M:%S %p", server_time)
 
 @app.route('/')
 def index():
@@ -17,43 +28,46 @@ def index():
     
     response = make_response(template)
     
-    response.headers['Cache-Control'] = 'public, max-age=300, s-maxage=600'
+    response.headers['Cache-Control'] = 'public, max-age=180, s-maxage=600'
     return response
 
+@app.route("/submit", methods=["POST"])
+def submit():
+    num_list = [str(i) for i in range(1000)]  # Simplified for demonstration
+    number = request.form.get("number")
+    if number in num_list:
+        position = num_list.index(number)
+        return jsonify({"number": number, "position": position}), 200
+    else:
+        return jsonify({"error": "Number not found in the list."}), 404
 
+@app.route('/transfer_images', methods=['POST'])
+def transfer_images():
+    try:
+        client = storage.Client()
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "server/src/GoogleKey/googlekey.json"
+        source_bucket_name = 'odissourcebucket'
+        destination_bucket_name = 'odisoutputbucket'
 
+        source_bucket = client.bucket(source_bucket_name)
+        destination_bucket = client.bucket(destination_bucket_name)
 
-@app.route('/filter_images', methods=['POST'])
-def filter_images():
-    source_bucket_name = request.form.get('source_bucket')
-    destination_bucket_name = request.form.get('destination_bucket')
+        blobs = source_bucket.list_blobs()
+        for blob in blobs:
+            if blob.content_type.startswith('image/'):
+                source_bucket.copy_blob(blob, destination_bucket, new_name=blob.name)
 
-    # Initialize the Google Cloud Storage client
-    storage_client = storage.Client()
-
-    # Get the source and destination buckets
-    source_bucket = storage_client.bucket(source_bucket_name)
-    destination_bucket = storage_client.bucket(destination_bucket_name)
-
-    if not source_bucket.exists() or not destination_bucket.exists():
-        return jsonify({"error": "Source or destination bucket does not exist."}), 400
-
-    filtered_images = []
-
-    blobs = source_bucket.list_blobs()
-
-    for blob in blobs:
-        if blob.name.lower().endswith(('.jpg', '.jpeg')):
-            # Copy the image from the source to the destination bucket
-            source_blob = source_bucket.blob(blob.name)
-            destination_blob = destination_bucket.blob(blob.name)
-            source_blob.download_to_filename('/tmp/temp_image.jpg')
-            destination_blob.upload_from_filename('/tmp/temp_image.jpg')
-            filtered_images.append(blob.name)
-
-    return jsonify({"message": "Images copied to the destination bucket.", "filtered_images": filtered_images})
+        response = jsonify(message='Images transferred successfully!')
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response, 200
     
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    except Exception as e:
+
+        app.logger.error('An internal error occurred: %s', e, exc_info=True)
+        response = jsonify(error='An internal server error occurred.')
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response, 500
+    
+    
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
