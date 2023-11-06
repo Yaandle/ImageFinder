0ac38f53428dc1460e45df11745d09d8d99ff1c5
Yaandle <131413645+Yaandle@users.noreply.github.com>
2023-11-07 10:13:07 +1100

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -4,17 +4,10 @@ from google.cloud import storage
 import os
 import json
 from google.oauth2 import service_account
+from google.cloud import secretmanager
+import json
 
 app = Flask(__name__)
-
-def get_storage_client():
-    creds_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-    if creds_json:
-        creds_info = json.loads(creds_json)
-        credentials = service_account.Credentials.from_service_account_info(creds_info)
-        return storage.Client(credentials=credentials)
-    else:
-        return storage.Client()
 
 def format_server_time():
     server_time = time.localtime()
@@ -33,13 +26,16 @@ def index():
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    num_list = [str(i) for i in range(1000)]  # Simplified for demonstration
+    num_list = [str(i) for i in range(1000)]  
     number = request.form.get("number")
     if number in num_list:
         position = num_list.index(number)
         return jsonify({"number": number, "position": position}), 200
     else:
         return jsonify({"error": "Number not found in the list."}), 404
+    
+def get_storage_client():
+    return storage.Client()
 
 @app.route('/transfer_images', methods=['POST'])
 def transfer_images():
@@ -67,7 +63,3 @@ def transfer_images():
         response = jsonify(error='An internal server error occurred.')
         response.headers['X-Content-Type-Options'] = 'nosniff'
         return response, 500
-    
-    
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
